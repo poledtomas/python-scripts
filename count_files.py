0db2b59d152d77b653bfe737342fb5e11071bdd9
Tomas Polednicek <poledto1@zenith.metacentrum.cz>
2025-02-05 18:57:06 +0100

    add count_files

---
+++ count_files.py
@@ -0,0 +1,18 @@
+import os
+
+
+def count_files_with_fin_oscar(directory):
+    count = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if "fin.oscar" in file:
+                count += 1
+    return count
+
+
+# Specify the directory to search
+directory_path = "/storage/brno12-cerit/home/poledto1/hydro/hybrid/sampler.out/lhc2760-80-90"  # Change this to the target directory
+
+# Count the files
+file_count = count_files_with_fin_oscar(directory_path)
+print(f"Number of files containing 'fin.oscar': {file_count}")
